b1e957533d1080ec8a7cbeb248fd3975a424823b
Sam Orr <samuelorr234@yahoo.com>
2018-06-29 19:51:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,10 @@ function logShout(string) { console.log('HELLO')}
 
 function logWhisper(string) {console.log('hello')}
 
-function sayHiToGrandma(string) {retu
+function sayHiToGrandma(string) { 
+if(string.equals(string.toLowerCase())){ return "I can't hear you!";}
+
+if(string.equals(string.toUpperCase())){ return "YES INDEED";}
+
+if(string.equals("I love you, Grandma")){ return "I love you, too."}
+}
